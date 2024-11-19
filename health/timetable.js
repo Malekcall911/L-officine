0573fdccf88bf04f6470dc3d1d827e766dc6a502
Malekcall911 <76880355+Malekcall911@users.noreply.github.com>
2024-11-19 16:41:10 +0100

    Add files via upload

---
+++ health/timetable.js
@@ -0,0 +1,133 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: 'free'
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '38-718',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '38-718',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '38-718',
+        subject: 'Asthma',
+        type: 'review'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '27-304Y',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '28-107',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '28-107',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'Asthma',
+        type: 'review'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '38-719',
+        subject: 'Asthma',
+        type: 'review'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '38-719',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'Asthma',
+        type: 'review'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '33-601',
+        subject: 'Asthma',
+        type: 'review'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '34-604',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '34-604',
+        subject: 'Asthma',
+        type: 'review'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '33-309',
+        subject: 'Asthma',
+        type: 'review'
+    }
+]
